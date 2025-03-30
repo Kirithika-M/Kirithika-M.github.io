9e7024095a0185d007740f742bb257a35330904f
Kirithika-M <kirithikagaji@gmail.com>
2025-03-30 21:19:00 +0530

    Updated all the files - 1

--- script.js
+++ script.js
@@ -7,3 +7,11 @@ function hideSidebar() {
     const sidebar = document.querySelector('.sidebar');
     sidebar.style.display = 'none';
 }
+
+// Code to create animated typing effect
+var typing = new Typed('.auto-typing', {
+    strings: ["am a CSE undergraduate", "am a Tech Enthusiast", "want to design good solutions"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true
+});
