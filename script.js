773c1154a74123622da8ba9c4aabf8a0e370e253
Kirithika-M <kirithikagaji@gmail.com>
2025-04-17 19:06:01 +0530

    Updated html, css and js file - 1

--- script.js
+++ script.js
@@ -15,3 +15,25 @@ var typing = new Typed('.auto-typing', {
     backSpeed: 100,
     loop: true
 });
+
+// Make sure this code is placed at the end of your script.js file
+function initHomeArrow() {
+    const homeArrowBtn = document.getElementById("home-arrow");
+    if (homeArrowBtn) {
+        homeArrowBtn.addEventListener("click", function() {
+            window.scrollTo({
+                top: 0,
+                behavior: 'smooth'
+            });
+        });
+    } else {
+        console.error("Home arrow button not found!");
+    }
+}
+
+// Make sure the DOM is fully loaded before running
+if (document.readyState === "loading") {
+    document.addEventListener("DOMContentLoaded", initHomeArrow);
+} else {
+    initHomeArrow();
+}
